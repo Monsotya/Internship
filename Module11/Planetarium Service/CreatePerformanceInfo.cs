83e942ef06efe694d69d5385d8716b66738beff0
Monsotya <sonya20033009@gmail.com>
2022-01-24 20:18:36 +0200

    Added classes

---
+++ Module11/Planetarium Service/CreatePerformanceInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planetarium_Service
+{
+    class CreatePerformanceInfo
+    {
+        private string title;
+        private string eventDescription;
+        private TimeSpan duration;
+        public CreatePerformanceInfo() { }
+        public CreatePerformanceInfo(string title, string eventDescription, TimeSpan duration)
+        {
+            this.title = title;
+            this.eventDescription = eventDescription;
+            this.duration = duration;
+        }
+    
+        public string Title { get => title; set => title = value; }
+        public string EventDescription { get => eventDescription; set => eventDescription = value; }
+        public TimeSpan Duration { get => duration; set => duration = value; }
+    }
+}
---
+++ Module11/Planetarium Service/PerformanceInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planetarium_Service
+{
+    class PerformanceInfo
+    {
+        private string performanceName;
+        private string performanceDetail;
+        private List<AvailablePlaces> availablePlaces;
+        public PerformanceInfo() { }
+        public PerformanceInfo(string performanceName, string performanceDetail, List<AvailablePlaces> availablePlaces) {
+            this.performanceName = performanceName;
+            this.performanceDetail = performanceDetail;
+            this.availablePlaces = availablePlaces;
+    }
+        public string PerformanceName { get => performanceName; set => performanceName = value; }
+        public string PerformanceDetail { get => performanceDetail; set => performanceDetail = value; }
+        internal List<AvailablePlaces> AvailablePlaces { get => availablePlaces; set => availablePlaces = value; }
+    }
+}
