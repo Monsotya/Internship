1051f2037b62a2e33bdab28ce1322fe6751041f3
Monsotya <sonya20033009@gmail.com>
2022-01-24 20:25:16 +0200

    Updated solution

--- Module11/Planetarium Service/PlanetariumService.cs
+++ Module11/Planetarium Service/PlanetariumService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Data;
 using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using Dapper;
 
 namespace Planetarium_Service
 {
@@ -14,10 +16,20 @@ namespace Planetarium_Service
             get { return connectionString; }
             set { connectionString = $"data source =.; database = {value}; integrated security = SSPI";  }
         }
-        public void CreatePoster(DateTime dateOfEvent, float price, int performanceId, int hallId)
+
+        public PlanetariumService()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        }
+        public void CreatePoster(List<DateTime> dateOfEvent, CreatePosterInfo infoPoster)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
+                if (dateOfEvent.Count == 0)
+                {
+                    throw new Exception("Date list must not be null!");
+                }
+
                 connection.Open();
 
                 SqlCommand cmd = new SqlCommand()
@@ -27,15 +39,73 @@ namespace Planetarium_Service
                     CommandType = CommandType.StoredProcedure
                 };
 
-                cmd.Parameters.AddWithValue("@dateOfEvent", dateOfEvent);
-                cmd.Parameters.AddWithValue("@price", price);
-                cmd.Parameters.AddWithValue("@performanceId", performanceId);
-                cmd.Parameters.AddWithValue("@hallId", hallId);
+                cmd.Parameters.AddWithValue("@dateOfEvent", dateOfEvent[0]);
+                cmd.Parameters.AddWithValue("@price", infoPoster.Price);
+                cmd.Parameters.AddWithValue("@performanceId", infoPoster.PerformanceId);
+                cmd.Parameters.AddWithValue("@hallId", infoPoster.HallId);
 
-                cmd.ExecuteNonQuery();
+                foreach(DateTime date in dateOfEvent)
+                {
+                    cmd.Parameters.Remove(cmd.Parameters["@dateOfEvent"]);
+                    cmd.Parameters.AddWithValue("@dateOfEvent", date);
+                    cmd.ExecuteNonQuery();
+                }
             }
         }
 
+        public void CreatePosterPerformance(List<DateTime> dateOfEvent, CreatePosterInfo infoPoster, CreatePerformanceInfo infoPerformance)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                if (dateOfEvent.Count == 0)
+                {
+                    throw new Exception("Date list must not be null!");
+                }
+                infoPoster.PerformanceId = CreatePerformance(infoPerformance);
+
+                if (infoPoster.PerformanceId == -1)
+                {
+                    throw new Exception("Error creating performance");
+                }
+
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand()
+                {
+                    CommandText = "CreatePoster",
+                    Connection = connection,
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@dateOfEvent", dateOfEvent[0]);
+                cmd.Parameters.AddWithValue("@price", infoPoster.Price);
+                cmd.Parameters.AddWithValue("@performanceId", infoPoster.PerformanceId);
+                cmd.Parameters.AddWithValue("@hallId", infoPoster.HallId);
+
+                foreach (DateTime date in dateOfEvent)
+                {
+                    cmd.Parameters.Remove(cmd.Parameters["@dateOfEvent"]);
+                    cmd.Parameters.AddWithValue("@dateOfEvent", date);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public int CreatePerformance(CreatePerformanceInfo info)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var sql = "INSERT INTO Performance (Title, Duration, EventDescription) VALUES (@Title, @Duration, @EventDescription)";
+                connection.Execute(sql, new { Title = info.Title, Duration = info.Duration, EventDescription = info.EventDescription });
+                SqlCommand cmd = new SqlCommand("SELECT MAX(Id) AS Id FROM Performance", connection);
+                connection.Open();
+                SqlDataReader sd = cmd.ExecuteReader();
+                while (sd.Read())
+                {
+                    return (int)sd["Id"];
+                }
+                return -1;
+            }
+        }
         public bool BuyTicket(int ticketId)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -54,13 +124,15 @@ namespace Planetarium_Service
                     return true;
                 return false;
 
-                /*SqlCommand cm = new SqlCommand($"select * from ticket where id = {ticketId}", connection);
+                /*SqlCommand cm = new SqlCommand($"select * from ticket where id = @TicketId", connection);
+                cmd.Parameters.AddWithValue("@TicketId", ticketId);
                 SqlDataReader sdr = cm.ExecuteReader();
                 while (sdr.Read()) {
                     if((string)sdr["TicketStatus"] == "available")
                     {
                         sdr.Close();
-                        SqlCommand cmd = new SqlCommand($"update ticket set ticketstatus = 'bought' where id = {ticketId}", connection);
+                        SqlCommand cmd = new SqlCommand($"update ticket set ticketstatus = 'bought' where id = @ticketId", connection);
+                        cmd.Parameters.AddWithValue("@TicketId", ticketId);
                         SqlDataReader sd = cmd.ExecuteReader();
                         return true;
                     }
@@ -68,7 +140,7 @@ namespace Planetarium_Service
                 return false;*/
             }
         }
-        public DataTable GetAvailablePerformances(DateTime from, DateTime to) 
+        public List<PerformanceInfo> GetAvailablePerformances(DateTime from, DateTime to) 
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -80,31 +152,44 @@ namespace Planetarium_Service
                 da.SelectCommand.Parameters.AddWithValue("@to", to);
                 DataTable performances = new DataTable();
                 da.Fill(performances);
-                return performances;
 
-                /*connection.Open();
-                SqlDataAdapter sda = new SqlDataAdapter($"SELECT DISTINCT DateOfEvent, HallName, Price, Title, Duration " +
-                    $"FROM Poster, Hall, Performance WHERE[Performance].Id = PerformanceId AND[Hall].Id = HallId " +
-                    $"AND DateOfEvent BETWEEN '{from.ToString("yyyy-MM-dd HH:mm:ss")}' AND '{to.ToString("yyyy-MM-dd HH:mm:ss")}'", connection);
-                DataTable performances = new DataTable();
-                sda.Fill(performances);
-                return performances;*/
+                SqlDataAdapter sda = new SqlDataAdapter("GetAvailablePlaces", connection);
+                sda.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                sda.SelectCommand.Parameters.AddWithValue("@from", from);
+                sda.SelectCommand.Parameters.AddWithValue("@to", to);
+                DataTable places = new DataTable();
+                List<AvailablePlaces> availablePlaces = new List<AvailablePlaces>();
+                List<PerformanceInfo> availablePerformances = new List<PerformanceInfo>();
+                sda.SelectCommand.Parameters.AddWithValue("@performance", "");
+
+                foreach (DataRow performance in performances.Rows)
+                {
+                    sda.SelectCommand.Parameters.Remove(sda.SelectCommand.Parameters["@performance"]);
+                    sda.SelectCommand.Parameters.AddWithValue("@performance", performance["Title"]);
+                    
+                    sda.Fill(places);
+                    var values = new DynamicParameters();
+                    values.Add("to", to, dbType: DbType.DateTime);
+                    values.Add("from", from, dbType: DbType.DateTime);
+                    values.Add("performance", performance["Title"], dbType: DbType.String);
+                    availablePlaces = connection.Query<AvailablePlaces>("GetAvailablePlaces", values, commandType: CommandType.StoredProcedure).ToList();
+
+                    availablePerformances.Add(new PerformanceInfo((string)performance["Title"], (string)performance["EventDescription"], new List<AvailablePlaces>(availablePlaces)));
+                    availablePlaces.Clear();
+                }
+
+                return availablePerformances;
             }
         }
-
-        public List<Dictionary<string, object>> RevokeOrders(DateTime from, DateTime to, int hallid, List<int> places)
+        public List<RevokeInfo> RevokeOrders(DateTime from, DateTime to, int hallid, List<int> places)
         {
             if (places.Count == 0)
             {
                 throw new Exception("Places must not be NULL!");
             }
 
-            StringBuilder placesString = new StringBuilder(places[0].ToString());
-            for (int i = 1; i < places.Count; i++)
-            {
-                placesString.Append(",");
-                placesString.Append(places[i]);
-            }
+            String placesString = String.Join(',', places);
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -112,20 +197,19 @@ namespace Planetarium_Service
 
                 SqlDataAdapter da = new SqlDataAdapter("GetRevokedOrders", connection);
                 da.SelectCommand.CommandType = CommandType.StoredProcedure;
-                List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
+                List<RevokeInfo> result = new List<RevokeInfo>();
 
                 da.SelectCommand.Parameters.AddWithValue("@from", from);
                 da.SelectCommand.Parameters.AddWithValue("@to", to);
-                da.SelectCommand.Parameters.AddWithValue("@places", placesString.ToString());
-                da.SelectCommand.Parameters.AddWithValue("@hallid", hallid);
+                da.SelectCommand.Parameters.AddWithValue("@places", placesString);
+                da.SelectCommand.Parameters.AddWithValue("@hallId", hallid);
                 DataTable orders = new DataTable();
 
                 da.Fill(orders);
                 foreach (DataRow order in orders.Rows)
                 {
-                    result.Add(new Dictionary<string, object> { { "OrderId", order["Id"] }, { "ClientEmail", order["Email"] }, { "InfoMessage",
-                            $"Dear client {order["ClientName"]}, we are sorry to inform that your order canceled due to maintance work on " +
-                            $"ordered place number {order["Place"]}, payment in amount {order["Price"]} will be returned to your bank account soon" } });
+                    result.Add(new RevokeInfo((int)order["Id"], (string)order["Email"], 
+                        GetInfoMessage((string)order["ClientName"], order["Place"].ToString(), order["Price"].ToString())));
                 }
 
                 SqlCommand cmd = new SqlCommand()
@@ -137,30 +221,55 @@ namespace Planetarium_Service
 
                 cmd.Parameters.AddWithValue("@from", from);
                 cmd.Parameters.AddWithValue("@to", to);
-                cmd.Parameters.AddWithValue("@places", places);
-                cmd.Parameters.AddWithValue("@hallid", hallid);
+                cmd.Parameters.AddWithValue("@places", placesString);
+                cmd.Parameters.AddWithValue("@hallId", hallid);
+
+                cmd.ExecuteNonQuery();
 
                 return result;
 
                 /*connection.Open();
 
-                List<Dictionary<string, object>> result =  new List<Dictionary<string, object>>();
-                SqlDataAdapter sda = new SqlDataAdapter($"SELECT Place, Price * Surcharge as Price, [Orders].Id, ClientName, Email FROM Ticket INNER JOIN Orders ON([Ticket].OrderId = [Orders].Id) INNER JOIN Poster ON([Ticket].PosterId = [Poster].Id) INNER JOIN Tier ON([Ticket].TierId = [Tier].Id) WHERE[Ticket].place in ({placesString}) AND(SELECT HallId FROM Poster WHERE Id = PosterId) = {hallid} AND (SELECT DateOfEvent FROM Poster WHERE Id = PosterId) BETWEEN '{from.ToString("yyyy-MM-dd HH:mm:ss")}' AND '{to.ToString("yyyy-MM-dd HH:mm:ss")}'", connection);
-                
+                List<RevokeInfo> result =  new List<RevokeInfo>();
+                SqlDataAdapter sda = new SqlDataAdapter($"SELECT Place, Price * Surcharge as Price, [Orders].Id, ClientName, " +
+                    $"Email FROM Ticket INNER JOIN Orders ON([Ticket].OrderId = [Orders].Id) INNER JOIN Poster ON([Ticket].PosterId " +
+                    $"= [Poster].Id) INNER JOIN Tier ON([Ticket].TierId = [Tier].Id) WHERE[Ticket].place in (SELECT CAST(value AS int) FROM STRING_SPLIT(@places, ',')) AND (SELECT HallId " +
+                    $"FROM Poster WHERE Id = PosterId) = @hallId AND (SELECT DateOfEvent FROM Poster WHERE Id = PosterId) " +
+                    $"BETWEEN @from AND @to", connection);
+
+                sda.SelectCommand.Parameters.AddWithValue("@from", from);
+                sda.SelectCommand.Parameters.AddWithValue("@to", to);
+                sda.SelectCommand.Parameters.AddWithValue("@places", placesString);
+                sda.SelectCommand.Parameters.AddWithValue("@hallId", hallid);
+
                 DataTable orders = new DataTable();
      
                 sda.Fill(orders);
                 foreach (DataRow order in orders.Rows)
                 {
-                    result.Add(new Dictionary<string, object> { { "OrderId", order["Id"] }, { "ClientEmail", order["Email"] }, { "InfoMessage", 
-                            $"Dear client {order["ClientName"]}, we are sorry to inform that your order canceled due to maintance work on " +
-                            $"ordered place number {order["Place"]}, payment in amount {order["Price"]} will be returned to your bank account soon" } });
+                    result.Add(new RevokeInfo((int)order["Id"], (string)order["Email"], 
+                        GetInfoMessage((string)order["ClientName"], order["Place"].ToString(), (string)order["Price"].ToString())));
                 }
-                SqlCommand cm = new SqlCommand($"UPDATE Ticket SET TicketStatus = 'unavailable', OrderId = NULL WHERE [Ticket].place in ({placesString}) AND (SELECT HallId FROM Poster WHERE Id = PosterId) = {hallid} AND (SELECT DateOfEvent FROM Poster WHERE Id = PosterId) BETWEEN '{from.ToString("yyyy-MM-dd HH:mm:ss")}' AND '{to.ToString("yyyy-MM-dd HH:mm:ss")}'", connection);
+                SqlCommand cm = new SqlCommand($"UPDATE Ticket SET TicketStatus = 'unavailable', OrderId = NULL WHERE " +
+                    $"[Ticket].place in (SELECT CAST(value AS int) FROM STRING_SPLIT(@places, ',')) AND (SELECT HallId FROM Poster WHERE Id = PosterId) = @hallId " +
+                    $"AND (SELECT DateOfEvent FROM Poster WHERE Id = PosterId) BETWEEN @from " +
+                    $"AND @to", connection);
+
+
+                cm.Parameters.AddWithValue("@from", from);
+                cm.Parameters.AddWithValue("@to", to);
+                cm.Parameters.AddWithValue("@places", placesString);
+                cm.Parameters.AddWithValue("@hallId", hallid);
+
                 SqlDataReader sdr = cm.ExecuteReader();
                 sdr.Close();
                 return result;*/
             }
-}
+        }        
+        public string GetInfoMessage(string ClientName, string Place, string Price)
+        {
+            return $"Dear client {ClientName}, we are sorry to inform that your order canceled due to maintance work on " +
+                   $"ordered place number {Place}, payment in amount {Price} will be returned to your bank account soon";
+        }
     }
 }
--- Module11/Planetarium Service/Program.cs
+++ Module11/Planetarium Service/Program.cs
@@ -8,11 +8,10 @@ namespace Planetarium_Service
         static void Main(string[] args)
         {
             PlanetariumService obj = new PlanetariumService();
-            obj.ConnectionString = "Planetarium";
             obj.BuyTicket(16);
             DateTime from = new DateTime(2022, 1, 30);
             DateTime to = new DateTime(2022, 2, 15);
-            obj.CreatePoster(to, 120F, 3, 3);
+            obj.CreatePosterPerformance(new List<DateTime> { to, from}, new CreatePosterInfo(0, 1, 100), new CreatePerformanceInfo("aa", "bbbbb", new TimeSpan(100000000000)));
             var result = obj.GetAvailablePerformances(from, to);
             var res = obj.RevokeOrders(from, to, 1, new List<int> { 1, 2, 3 });
         }
