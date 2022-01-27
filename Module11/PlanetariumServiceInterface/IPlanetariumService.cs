1e5dad9f43216b5e35ee4c16746695d3e41a7a88
Monsotya <sonya20033009@gmail.com>
2022-01-27 13:08:06 +0200

    Added PlanetariumService interface

---
+++ Module11/PlanetariumServiceInterface/IPlanetariumService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using PlanetariumServiceModules;
+
+namespace PlanetariumServiceInterface
+{
+    public interface IPlanetariumService
+    {
+        string ConnectionString { get; set; }
+        bool BuyTicket(int ticketId);
+        int CreatePerformance(CreatePerformanceInfo info);
+        void CreatePoster(List<DateTime> dateOfEvent, CreatePosterInfo infoPoster);
+        void CreatePosterPerformance(List<DateTime> dateOfEvent, CreatePosterInfo infoPoster, CreatePerformanceInfo infoPerformance);
+        List<PerformanceInfo> GetAvailablePerformances(DateTime from, DateTime to);
+        string GetInfoMessage(string ClientName, string Place, string Price);
+        List<RevokeInfo> RevokeOrders(DateTime from, DateTime to, int hallid, List<int> places);
+    }
+}
