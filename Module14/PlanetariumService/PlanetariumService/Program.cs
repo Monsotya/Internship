d080ed21d29b42909b27ecac2b68d606451fa7b7
Monsotya <sonya20033009@gmail.com>
2022-02-17 23:54:30 +0200

    Update Program.cs

--- Module14/PlanetariumService/PlanetariumService/Program.cs
+++ Module14/PlanetariumService/PlanetariumService/Program.cs
@@ -1,7 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using PlanetariumService.Models;
+using PlanetariumService.Repositories;
+using PlanetariumService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<PlanetariumServiceContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PlanetariumServiceContext"), builder => builder.EnableRetryOnFailure()));
+builder.Services.AddTransient<PlanetariumServiceContext>();
+
+builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddTransient<ITicketRepository, TicketRepository>();
+builder.Services.AddTransient<IPosterRepository, PosterRepository>();
+builder.Services.AddTransient<IHallRepository, HallRepository>();
+builder.Services.AddTransient<ITicketRepository, TicketRepository>();
+
+builder.Services.AddTransient<ITicketService, TicketService>();
+builder.Services.AddTransient<IPosterService, PosterService>();
+builder.Services.AddTransient<IHallService, HallService>();
+builder.Services.AddTransient<IPerformanceService, PerformanceService>();
+
+builder.Host.UseDefaultServiceProvider(o =>
+{
+    o.ValidateOnBuild = true;
+    o.ValidateScopes = true;
+});
+
 
 var app = builder.Build();
 
