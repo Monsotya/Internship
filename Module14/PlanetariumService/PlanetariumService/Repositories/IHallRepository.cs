388d000b0fa7f51f92d7b39f4e95bb105ed09922
Monsotya <sonya20033009@gmail.com>
2022-02-17 23:54:23 +0200

    Added repositories

---
+++ Module14/PlanetariumService/PlanetariumService/Repositories/IHallRepository.cs
@@ -0,0 +1,11 @@
+﻿using PlanetariumService.Models;
+
+namespace PlanetariumService.Repositories
+{
+    public interface IHallRepository
+    {
+        Task<Hall> GetByIdAsync(int id);
+
+        public List<Hall> GetAll();
+    }
+}
---
+++ Module14/PlanetariumService/PlanetariumService/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PlanetariumService.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+        IQueryable<TEntity> GetAll();
+        Task<TEntity> UpdateAsync(TEntity entity);
+    }
+}
