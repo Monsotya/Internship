b3103eb430307c07826723af5646f263236fb219
Monsotya <sonya20033009@gmail.com>
2022-02-22 13:03:37 +0200

    Added program files

--- Module15/PlanetariumService/PlanetariumService/Program.cs
+++ Module15/PlanetariumService/PlanetariumService/Program.cs
@@ -1,3 +1,10 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PlanetariumModels;
+using PlanetariumRepositories;
+using PlanetariumService.Profiles;
+using PlanetariumServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new OneProfile());
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+
+builder.Services.AddDbContext<PlanetariumServiceContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PlanetariumServiceContext"), builder => builder.EnableRetryOnFailure()));
+builder.Services.AddTransient<PlanetariumServiceContext>();
+
+builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddTransient<ITicketRepository, TicketRepository>();
+builder.Services.AddTransient<IPosterRepository, PosterRepository>();
+builder.Services.AddTransient<IHallRepository, HallRepository>();
+builder.Services.AddTransient<ITicketRepository, TicketRepository>();
+
+builder.Services.AddTransient<ITicketService, TicketService>();
+builder.Services.AddTransient<IPosterService, PosterService>();
+builder.Services.AddTransient<IHallService, HallService>();
+builder.Services.AddTransient<IPerformanceService, PerformanceService>();
+
+builder.Host.UseDefaultServiceProvider(o =>
+{
+    o.ValidateOnBuild = true;
+    o.ValidateScopes = true;
+});
+
+
 
 var app = builder.Build();
 
